42e67c9b04e6e142c6e5d5d8e62b99440d604780
ernokanst <ernokanst@gmail.com>
2023-06-20 23:26:20 +0300

    Настройка Django

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, StoreItem
+
+admin.site.register(StoreItem)
+admin.site.register(Category)
--- backend/store/models.py
+++ backend/store/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category_name
+
+
+class StoreItem(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    description = models.TextField()
+    price = models.IntegerField(default=0)
+    image = models.ImageField()
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name

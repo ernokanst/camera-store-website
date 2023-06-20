1ec9307987984bc1e96c66eb494d6a50fe4c3132
ernokanst <ernokanst@gmail.com>
2023-06-20 22:13:13 +0300

    Улучшенная интеграция

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates', '../camera-store/dist'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 MEDIA_URL = "/"
-MEDIA_ROOT = "store/templates/store/"
+MEDIA_ROOT = "../camera-store/dist"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
